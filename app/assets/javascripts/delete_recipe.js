c5842d6cab4f9729002c456e058a0a1985ce90e5
Adam Penner <penner.adam@gmail.com>
2023-06-20 18:58:29 -0600

    Still working on delete

--- app/assets/javascripts/delete_recipe.js
+++ app/assets/javascripts/delete_recipe.js
@@ -1,5 +1,6 @@
 document.addEventListener('turbo:load', function() {
   // Listen for the 'turbo:load' event triggered by Turbo
+
   var deleteButton = document.getElementById('delete-recipe');
   // Get the delete button element by its ID
 
@@ -8,17 +9,24 @@ document.addEventListener('turbo:load', function() {
 
     deleteButton.addEventListener('click', function(event) {
       // Add a click event listener to the delete button
+
       event.preventDefault();
       // Prevent the default behavior of the click event (navigation)
+
+      console.log('Delete button clicked');
+      // Log a message to the console for debugging
 
       if (confirm('Are you sure you want to delete this recipe?')) {
         // Display a confirmation dialog and proceed if confirmed
 
+        console.log('Confirmation accepted');
+        // Log a message to the console for debugging
+
         var form = document.createElement('form');
         // Create a new form element
 
-        form.method = 'POST';
-        // Set the HTTP method of the form to 'POST'
+        form.method = 'DELETE';
+        // Set the HTTP method of the form to 'DELETE'
 
         form.action = this.href;
         // Set the form action to the URL of the delete button
@@ -38,11 +46,11 @@ document.addEventListener('turbo:load', function() {
         document.body.appendChild(form);
         // Append the form to the document body
 
-        console.log(form);
-        // Log the form object to the console for debugging
-
         form.submit();
         // Submit the form (triggering the delete action)
+      } else {
+        console.log('Confirmation rejected');
+        // Log a message to the console for debugging
       }
     });
   }
